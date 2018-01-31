b8ef966bca02f41e2f7f3301f5c08b7fd63504bf
Igor Fedyukin <igor_fedyukin@epam.com>
2018-01-31 18:49:23 +0300

    Fixed "isAction" helper

--- src/helpers.js
+++ src/helpers.js
@@ -3,4 +3,4 @@ import { SymbolDispatch, SymbolGetActionTypes } from './symbols';
 export const combineListeners = (state, listeners = []) => () => listeners.forEach(fn => fn(state));
 export const isReducer = reducer => ['subscribe', 'getState', SymbolDispatch, SymbolGetActionTypes]
     .every(key => typeof reducer[key] === 'function');
-export const isAction = ({ type }) => (typeof a === 'object' && a !== null && a.type);
+export const isAction = action => (typeof action === 'object' && action !== null && action.type);
