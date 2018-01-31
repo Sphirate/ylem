f1f88d920cdd8a908a19db72511bed4e376bc9a4
Dmitry Rykov <sphirate@gmail.com>
2018-01-31 19:59:45 +0300

    Add build tools

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+const baseConfig = {
+    input: './src/index.js',
+    output: {
+        name: 'ylem',
+        file: './lib/index.js',
+        format: 'umd',
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+    ],
+};
+const minConfig = {
+    input: './src/index.js',
+    output: {
+        name: 'ylem',
+        file: './lib/index.min.js',
+        format: 'umd',
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        uglify(),
+    ],
+};
+
+export default [baseConfig, minConfig];
