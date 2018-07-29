d31c19d81f890ebb58c192b12743f9fb8aacc7e3
Sphirate Thremer <sphirate@gmail.com>
2018-07-29 22:15:42 +0300

    Update CombineReducer

--- src/combine-reducers.js
+++ src/combine-reducers.js
@@ -1,10 +1,25 @@
 export const combineReducers = (reducersMap) => {
+    if (!(reducersMap instanceof Object)) {
+        throw new Error('Reducers map should be of Object subclass type');
+    }
     const entries = Object.entries(reducersMap);
-    const reducers = Object.values(reducersMap);
 
-    const reducer = () => entries.reduce((acc, [key, fn]) => ({ ...acc, [key]: fn() }), {});
+    if (entries.length === 0) {
+        throw new Error('Reducer map is empty. Check iterator property of your map');
+    }
+    entries.forEach(([key, reducer]) => {
+        if (typeof reducer !== 'function' || typeof reducer.subscribe !== 'function') {
+            throw new Error(`Given reducers map contain non-reducer value in ${key} key`);
+        }
+    });
+
+    const constructor = reducersMap.constructor || reducersMap.__proto__.constructor; // eslint-disable-line
+    const reducer = () => entries.reduce((acc, [key, fn]) => {
+        acc[key] = fn();
+        return acc;
+    }, constructor());
     reducer.subscribe = (listener) => {
-        const unsubscribers = reducers.map(innerReducer => innerReducer.subscribe(listener));
+        const unsubscribers = entries.map(([, innerReducer]) => innerReducer.subscribe(listener));
         return () => unsubscribers.forEach(fn => fn());
     };
 
